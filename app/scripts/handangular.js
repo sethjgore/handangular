babc25ea4f8d2b7469659a9c3dbf8ab5fa7cdcf0
sethjgore <sethjgore@gmail.com>
2014-09-25 13:52:44 -0500

    first commit

---
+++ app/scripts/handangular.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var handangular = angular.module('handangular', [
+  'ui.router',
+  ]);
+
+handangular.config(function ($stateProvider, $urlRouterProvider) {
+
+
+    $urlRouterProvider.otherwise('/projects');
+
+    $stateProvider
+      .state('/about', {
+        url: '/about',
+        templateUrl: '/views/about.html',
+        controller: 'ProjectsCtrl'
+      })
+      .state('projects', {
+        abstract: true,
+        url: '/projects',
+        templateUrl: '/views/projects.html',
+      })
+      .state('projects.index', {
+        url:'',
+        templateUrl: '/views/projects.index.html',
+        controller: 'ProjectsCtrl',
+      })
+      .state('projects.list', {
+        url: '/{project}',
+        templateUrl: '/views/projects.list.html',
+        controller: 'ProjectsCtrl',
+        resolve: {
+          project: ['$stateParams', function($stateParams){
+            return $stateParams.project;
+          }]
+        }
+      })
+      .state('projects.list.content', {
+        url: '/content/{section}',
+        templateUrl: '/views/projects.list.content.html',
+        controller: 'ProjectsCtrl',
+        resolve: {
+          section: ['$stateParams', function($stateParams){
+            return $stateParams.section;
+          }]
+        }
+      })
+      .state('projects.list.video', {
+        url: '/video',
+        templateUrl: '/views/projects.list.video.html',
+        controller: 'ProjectsCtrl'
+
+      });
+
+
+});
---
+++ app/scripts/projects/projects-ctrl.js
@@ -0,0 +1,59 @@
+'use strict';
+
+handangular.controller('ProjectsCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
+    $scope.projects = [
+      {
+        title: 'Research',
+        subtitle: 'Some brief comment about the project',
+        sections: [
+          {
+            title: 'Burning Question',
+            content: 'orem ipsum dolor sit amet',
+          },
+          {
+            title: 'Methodology',
+            content: 'orem ipsum dolor sit amet',
+          },
+          {
+            title: 'Data Collection',
+            content: 'orem ipsum dolor sit amet',
+          },
+          {
+            title: 'Results/Discussion',
+            content: 'orem ipsum dolor sit amet',
+          },
+          {
+            title: 'Further Research',
+            content: 'orem ipsum dolor sit amet',
+          },
+        ]
+      },
+      {
+        title: 'Growth',
+        subtitle: 'Some brief comment about the project',
+        content: 'Hello? How are you? I am doing fine.'
+      },
+      {
+        title: 'Vision',
+        subtitle: 'Some brief comment about the project',
+        content: 'Hello? How are you? I am doing fine.'
+      },
+      {
+        title: 'Beliefs',
+        subtitle: 'Some brief comment about the project',
+        content: 'Hello? How are you? I am doing fine.'
+      },
+      {
+        title: 'Goals',
+        subtitle: 'Some brief comment about the project',
+        content: 'Hello? How are you? I am doing fine.'
+      },
+      ];
+
+      $scope.project = $scope.projects[$stateParams.project];
+      if($stateParams.section){
+        $scope.section = $scope.project.sections[$stateParams.section];
+      }
+      console.log($stateParams);
+
+}]);
