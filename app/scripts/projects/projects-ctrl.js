5e2863e1c9a072cfc807c98c3c528c926b0513be
sethjgore <sethjgore@gmail.com>
2014-09-30 15:44:02 -0500

    added section text

--- app/scripts/projects/projects-ctrl.js
+++ app/scripts/projects/projects-ctrl.js
@@ -6,26 +6,52 @@ handangular.controller('ProjectsCtrl', ['$scope', '$stateParams', function ($sco
       {
         title: 'Research',
         subtitle: 'Some brief comment about the project',
+        content: 'We have several sections to our project. Some content about current section. Our project is focused on collecting some kind of data that will be. Engineered to accomodate some criteria arbitrarily decided by committees and product owners.',
         sections: [
           {
             title: 'Burning Question',
-            content: 'orem ipsum dolor sit amet',
+            content: 'Quick summary',
+            show: false,
+            coords: {
+              x : '16',
+              y : '1'
+            }
           },
           {
             title: 'Methodology',
-            content: 'orem ipsum dolor sit amet',
+            content: 'Some summary',
+            show: false,
+            coords: {
+              x : '27',
+              y : '11'
+            }
           },
           {
             title: 'Data Collection',
-            content: 'orem ipsum dolor sit amet',
+            content: 'About this section',
+            show: false,
+            coords: {
+              x : '28',
+              y : '16'
+            }
           },
           {
             title: 'Results/Discussion',
             content: 'orem ipsum dolor sit amet',
+            show: false,
+            coords: {
+              x : '25',
+              y : '21'
+            }
           },
           {
             title: 'Further Research',
-            content: 'orem ipsum dolor sit amet',
+            content: 'What will happen after',
+            show: false,
+            coords: {
+              x : '20',
+              y : '26'
+            }
           },
         ]
       },
@@ -51,7 +77,12 @@ handangular.controller('ProjectsCtrl', ['$scope', '$stateParams', function ($sco
       },
       ];
 
+      $scope.hover = function(node) {
+        return node.show = ! node.show;
+      }
+
       $scope.project = $scope.projects[$stateParams.project];
+
       if($stateParams.section){
         $scope.section = $scope.project.sections[$stateParams.section];
       }
