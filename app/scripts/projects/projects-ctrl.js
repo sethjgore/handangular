16446537e55d5d13f2418c66b0f87b2120c52939
sethjgore <sethjgore@gmail.com>
2014-10-08 10:41:03 -0500

    pushing foward

--- app/scripts/projects/projects-ctrl.js
+++ app/scripts/projects/projects-ctrl.js
@@ -10,6 +10,17 @@ handangular.controller('ProjectsCtrl', ['$scope', '$stateParams', function ($sco
         color: 'blue',
         sections: [
           {
+            title: '',
+            subtitle: '',
+            content: '',
+            video: '',
+            show: false,
+            coords: {
+              x : '999999',
+              y : '999999'
+            }
+          },
+          {
             title: 'Burning Question',
             subtitle: 'Questions that we wanted to ask',
             content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
