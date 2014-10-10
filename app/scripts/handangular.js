37288ba3f7f0cb5d58360ae0fc74d8fcbebcd5ef
sethjgore <sethjgore@gmail.com>
2014-10-09 19:25:16 -0500

    working on subdirectory

--- app/scripts/handangular.js
+++ app/scripts/handangular.js
@@ -12,22 +12,22 @@ handangular.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('/about', {
         url: '/about',
-        templateUrl: '/partials/about.html',
+        templateUrl: 'partials/about.html',
         controller: 'ProjectsCtrl'
       })
       .state('projects', {
         abstract: true,
         url: '/projects',
-        templateUrl: '/partials/projects.html',
+        templateUrl: 'partials/projects.html',
       })
       .state('projects.index', {
         url:'',
-        templateUrl: '/partials/projects.index.html',
+        templateUrl: 'partials/projects.index.html',
         controller: 'ProjectsCtrl',
       })
       .state('projects.list', {
         url: '/{project}',
-        templateUrl: '/partials/projects.list.html',
+        templateUrl: 'partials/projects.list.html',
         controller: 'ProjectsCtrl',
         resolve: {
           project: ['$stateParams', function($stateParams){
@@ -37,12 +37,12 @@ handangular.config(function ($stateProvider, $urlRouterProvider) {
       })
       .state('projects.list.index', {
         url: '',
-        templateUrl: '/partials/projects.list.index.html',
+        templateUrl: 'partials/projects.list.index.html',
         controller: 'ProjectsCtrl',
       })
       .state('projects.list.preview', {
         url: '/preview/{section}',
-        templateUrl: '/partials/projects.list.preview.html',
+        templateUrl: 'partials/projects.list.preview.html',
         controller: 'ProjectsCtrl',
         resolve: {
           section: ['$stateParams', function($stateParams){
@@ -52,7 +52,7 @@ handangular.config(function ($stateProvider, $urlRouterProvider) {
       })
       .state('projects.section', {
         url: '/{project}/section/{section}',
-        templateUrl: '/partials/projects.section.html',
+        templateUrl: 'partials/projects.section.html',
         controller: 'ProjectsCtrl',
         resolve: {
           section: ['$stateParams', function($stateParams){
